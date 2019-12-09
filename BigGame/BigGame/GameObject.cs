4da84d27b56110d99a7f79f200de15f3bb749205
kj cui <cui_acui@163.com>
2019-12-09 19:59:54 +0800

    updata

--- BigGame/BigGame/GameObject.cs
+++ BigGame/BigGame/GameObject.cs
@@ -40,7 +40,7 @@ namespace BigGame
                     {
                         OnMyValueChanged();
                     }       
-                                      
+                                       
                 }                
             }
         }
