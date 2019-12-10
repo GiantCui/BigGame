a52e20c3595386ce2c9f48c0492722aa9a573e10
Renaming <1019084152@qq.com>
2019-12-10 21:20:21 +0800

    增加吃金币类

---
+++ BigGame/BigGame/Action/Hero_GetGold.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigGame.Action
+{
+    public class Hero_GetGold
+    {
+         
+    }
+}
